0bb72fb8da3e126e75cc07c3e160d96b08251243
Ally <bagkingally@gmail.com>
2017-08-20 14:12:37 +0100

    Clicking on question now displays the answers. Not very happy with the resizing though.

--- flash.py
+++ flash.py
@@ -4,12 +4,18 @@ from tkinter import ttk
 root = Tk()
 root.title("Flash Cards")
 
-question = ttk.Label(root, text="Question", anchor="center")
-answer   = ttk.Label(root, text="Answer", anchor="center")
-wrong    = ttk.Button(root, text="Wrong")
-confused = ttk.Button(root, text="Mixup")
-again    = ttk.Button(root, text="Ask again")
-correct  = ttk.Button(root, text="Correct")
+qFrame = ttk.Frame(root)
+aFrame   = ttk.Frame(root)
+
+
+question = ttk.Label(qFrame, text="Question", anchor="center")
+answer   = ttk.Label(aFrame, text="Answer", anchor="center")
+wrong    = ttk.Button(aFrame, text="Wrong")
+confused = ttk.Button(aFrame, text="Mixup")
+again    = ttk.Button(aFrame, text="Ask again")
+correct  = ttk.Button(aFrame, text="Correct")
+
+qFrame.pack()
 
 question.grid(row=0, column=0, sticky=(E, W), columnspan=4)
 answer.grid(row=1, column=0, sticky=(E, W), columnspan=4)
@@ -20,7 +26,7 @@ again.grid(row=2, column=2, sticky=(E, W))
 correct.grid(row=2, column=3, sticky=(E, W))
 
 def showAnswers():
-	pass
+	aFrame.pack()
 
 question.bind('<1>', lambda e: showAnswers())
 root.mainloop()
